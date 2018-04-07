98fd3703b1758913411bc930a649d91e10584219
jingtang <jingtang0216@163.com>
2018-04-07 21:24:49 +0800

    通用结果修改

--- src/main/java/com/auth/MyauthApplication.java
+++ src/main/java/com/auth/MyauthApplication.java
@@ -1,15 +1,17 @@
-package com.auth.myauth;
+package com.auth;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ComponentScan
 @EnableAutoConfiguration
 @MapperScan("com.auth.*.dao")
+@EnableTransactionManagement
 public class MyauthApplication {
 
 	public static void main(String[] args) {
