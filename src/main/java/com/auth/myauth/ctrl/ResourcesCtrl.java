cd09c10c2a35cfe1d240da53c2b32e79a56fa70e
jingtang <jingtang0216@163.com>
2018-04-09 15:10:10 +0800

    待开发接口

--- src/main/java/com/auth/myauth/ctrl/ResourcesCtrl.java
+++ src/main/java/com/auth/myauth/ctrl/ResourcesCtrl.java
@@ -9,4 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("resources")
 public class ResourcesCtrl {
 
+	//添加资源
+	
+	//删除资源
+	
+	//更新资源
+	
+	//返回所有的资源树
+	
+	//根据ID资源树ID返回该资源下所有资源
+	
+	
 }
